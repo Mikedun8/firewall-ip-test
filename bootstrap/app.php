071e98c594935ea4be1ca8ecaa332f80d5e4b7ec
Mikmoal <miguel.moam@outlook.com>
2024-03-27 17:08:53 -0600

    trustproxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckGeoLocation;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->append(CheckGeoLocation::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
